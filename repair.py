232b670e2e189ada9ae57e2d8f362ef34c0d045a
Steve L. Nyemba <nyemba@gmail.com>
2016-01-14 09:26:10 -0600

    Refactoring the repair utility to work in a virtual environment as well as adding the system requirements/dependencieswq

--- repair.py
+++ repair.py
@@ -1,5 +1,26 @@
 """
-	This script is designed to repair character delimited files using anomaly detection under certain assumptions.
-	The assumptions will allow the script to run in various modes with varying levels  of overhead
-	
+	Steve L. Nyemba<steve@the-phi.com>
+	https://the-phi.com, The Phi Technology LLC
+
+	This file implements a character delimited file repair approach based on anomaly detection & ensemble learning across n-features in the dataset
+	The reason for this implementation is to streamline data ingestion, in the process of repairing and assessing the data this engine will provide quantitative assessment of the data
+	The quantitative assessment of the data is designed to determine what kind of processing this data would lend itself to
+
+	DESIGN:
+		- Sample records from which we derive statistics
+			a. The assumption here is that most records are properly submitted
+			b. A dataset has structural consistency
+		- From the sample we will derive the following:
+			a. Probability/Frequency of a field having data
+			b. Field length on average
+			c. Field type= {integer, double,date}
+			d. 
+		- Additional features will include scrubbing the data for non-ascii characters and extra whitespaces
+		This should in theory allow for a clean output that is easy to ingest. 
+
+		The code can be applied in either filter-mode or repair-mode:
+			- Filter mode is a passive mode (recommended if data loss is acceptable)
+			- Repair mode is designed to minimize data loss
+
 """
+
